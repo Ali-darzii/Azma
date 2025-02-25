a80990b2693c29926f4c599d2ea08337c7a189be
ali <ali.darzi.1354@gmail.com>
2025-02-25 04:23:22 +0330

    long time running error handling & better logg provided

--- src/server/controller.py
+++ src/server/controller.py
@@ -2,27 +2,19 @@ import subprocess
 from tabnanny import check
 
 
-def execute_os_command(command_name, parameters):
-    execute_time_out = 5
-    # todo: not
+def execute_os_command(command_name, parameters, time_out=5):
     result = subprocess.check_output([command_name] + parameters,
                                      stderr=subprocess.STDOUT,
                                      shell=False,
                                      text=True,
+                                     timeout=time_out
                                      )
     return result
-    # except subprocess.CalledProcessError as e:
-    #     return e.output
-    # except subprocess.TimeoutExpired:
-    #     return "Command timed out."
-    # except Exception as e:
-    #     return e
-    # return result
 
 
 def evaluate_math_expression(expression):
     try:
         result = eval(expression)
-        return str(result)
+        return int(result)
     except Exception as e:
         return f"Error evaluating math expression: {str(e)}"
--- src/server/thread.py
+++ src/server/thread.py
@@ -1,3 +1,5 @@
+import subprocess
+from src.server.manager import logger
 from src.utils.error_responses import ErrorResponse
 from src.server.controller  import execute_os_command, evaluate_math_expression
 import zmq
@@ -10,12 +12,18 @@ def handle_request(worker_url, context: zmq.Context = None):
     socket.connect(worker_url)
     while True:
         data = socket.recv_json()
+        logger.info(f"Received: {data}")
         command_type = data.get("command_type")
         if command_type == "os":
-            result = execute_os_command(data.get("command_name",[]), data.get("parameters",[]))
-            socket.send_json({"result": result})
+            try:
+                result = execute_os_command(data.get("command_name",[]), data.get("parameters",[]))
+                socket.send_json({"result": result})
+            except subprocess.TimeoutExpired:
+                socket.send_json(ErrorResponse.LongRunning)
+            except Exception as e:
+                socket.send_json({"error": e})
         elif command_type == "compute":
-            result = evaluate_math_expression(data)
+            result = evaluate_math_expression(data.get("expression"))
             socket.send_json({"result": result})
         else:
             socket.send_json(ErrorResponse.OsOrCompute)
